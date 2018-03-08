ec719c935d8379780a2292da32318e8b111e9f10
浮生i若梦o <89925977@qq.com>
2018-03-08 16:21:03 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/ws/flcweather/db/County.java
@@ -0,0 +1,13 @@
+package com.ws.flcweather.db;
+
+/**
+ * <pre>
+ *     author : Administrator
+ *     e-mail : 89925977@qq.com
+ *     time   : 2018/03/08
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class County {
+}
