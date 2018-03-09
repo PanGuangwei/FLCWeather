d7de56090e549d8ba218857495109c23b6765655
浮生i若梦o <89925977@qq.com>
2018-03-09 20:10:54 +0800

    完成遍历

---
+++ app/src/main/java/com/ws/flcweather/util/ToastUtil.java
@@ -0,0 +1,13 @@
+package com.ws.flcweather.util;
+
+/**
+ * <pre>
+ *     author : Administrator
+ *     e-mail : 89925977@qq.com
+ *     time   : 2018/03/09
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class ToastUtil {
+}
---
+++ app/src/main/java/com/ws/flcweather/util/Utility.java
@@ -0,0 +1,13 @@
+package com.ws.flcweather.util;
+
+/**
+ * <pre>
+ *     author : Administrator
+ *     e-mail : 89925977@qq.com
+ *     time   : 2018/03/08
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class Utility {
+}
